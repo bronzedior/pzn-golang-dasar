d0bf9dc8bd08ac9d4af342fa46f7b4a5be400488
Fransiscus Bronzedior <zediordonny@gmail.com>
2024-02-01 21:24:58 +0700

    penempatan append pada variadic function

--- variadic_function.go
+++ variadic_function.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
+func PrependItems(slice []int, values ...int) []int {
+	// return append(slice, values...)	//append in the back
+	return append(values, slice...)	//append in the front
+}
+
 func sumAll(numbers ...int) int {	//non-fixed number of arguments
 	total := 0
 
@@ -13,6 +18,10 @@ func sumAll(numbers ...int) int {	//non-fixed number of arguments
 }
 
 func main() {
+	slice := []int{3, 4, 5}
+	newSlice := PrependItems(slice, 1, 2, 3)
+	fmt.Println(newSlice)
+
 	fmt.Println(sumAll(10, 10, 10))
 	fmt.Println(sumAll(10, 10, 10, 10, 10, 10))
 	fmt.Println(sumAll(10, 10, 10, 10, 10, 10, 10, 10, 10))

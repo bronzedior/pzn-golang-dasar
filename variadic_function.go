059dd9e49ae0e67993e0c7d74d6a5886dc4aec67
Fransiscus Bronzedior <zediordonny@gmail.com>
2024-02-01 21:47:29 +0700

    RemoveItem di variadic function

--- variadic_function.go
+++ variadic_function.go
@@ -7,6 +7,13 @@ func PrependItems(slice []int, values ...int) []int {
 	return append(values, slice...)	//append in the front
 }
 
+func RemoveItem(slice []int, index int) []int {
+	if index < 0 || index >= len(slice) {
+		return slice
+	}
+	return append(slice[:index], slice[index+1:]...)
+}
+
 func sumAll(numbers ...int) int {	//non-fixed number of arguments
 	total := 0
 
@@ -21,6 +28,11 @@ func main() {
 	slice := []int{3, 4, 5}
 	newSlice := PrependItems(slice, 1, 2, 3)
 	fmt.Println(newSlice)
+
+	mySlice := []int{1, 2, 3, 4, 5}
+	fmt.Println("Original Slice:", mySlice)
+	mySlice = RemoveItem(mySlice, 2)
+	fmt.Println("Modified Slice:", mySlice)
 
 	fmt.Println(sumAll(10, 10, 10))
 	fmt.Println(sumAll(10, 10, 10, 10, 10, 10))
